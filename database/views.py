0400bbfaf0e832ef6fec9c87a8ae827bf143dd30
Azmi Renalji <azmirenalji4@gmail.com>
2024-12-24 08:38:38 +0700

    Landing-page

--- database/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("selamat datang di halaman database!")
-
-# Create your views here.
